36e408ef038a4e0cd32a5739ceb312fb319e584c
Helder Iury <iurytavares14@gmail.com>
2020-06-08 10:16:20 -0300

    Create 1008.py
    Uri 1008

---
+++ Beginner/Python/1008.py
@@ -0,0 +1,10 @@
+number = int(input())
+horas = int(input())
+horas_trabalhadas = float(input())
+
+salario = float(horas*horas_trabalhadas)
+
+print('NUMBER = {}'.format(number))
+
+print('SALARY = U$ {:.2F}'.format(salario))
+
